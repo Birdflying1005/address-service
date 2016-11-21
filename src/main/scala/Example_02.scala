0365fb934fac407c1f54f537e221cb47b585d4b5
focusj.x <focusj.x@gmail.com>
2016-11-21 09:42:53 +0800

    refector

--- src/main/scala/Example_02.scala
+++ src/main/scala/Example_02.scala
@@ -3,7 +3,7 @@ import java.nio.file.{Files, Path, Paths}
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{FileIO, Flow, Framing, GraphDSL, RunnableGraph, Sink, Source}
+import akka.stream.scaladsl.{FileIO, Flow, Framing, GraphDSL, Sink, Source}
 import akka.stream.{ActorMaterializer, ClosedShape, FlowShape}
 import akka.util.ByteString
 
@@ -18,62 +18,62 @@ object Example_02 extends App {
 
   import system.dispatcher
 
-  RunnableGraph.fromGraph(
-    GraphDSL.create() { implicit b ⇒
-      val citiesData: Flow[Path, Iterable[(String, (String, Path))], NotUsed] = Flow[Path].map(path ⇒
-        Files.newDirectoryStream(path).flatMap { statePath ⇒
-          val state = statePath.getFileName.toString.toUpperCase()
-          val country = path.getFileName.toString.toUpperCase()
+  val citiesData: Flow[Path, Iterable[(String, (String, Path))], NotUsed] = Flow[Path].map(path ⇒
+    Files.newDirectoryStream(path).flatMap { statePath ⇒
+      val state = statePath.getFileName.toString.toUpperCase()
+      val country = path.getFileName.toString.toUpperCase()
 
-          val filter = new Filter[Path]() {
-            override def accept(path: Path): Boolean = {
-              path.getFileName.toString.endsWith(".csv")
-            }
-          }
-          Files.newDirectoryStream(statePath, filter).map(p ⇒ country -> (state -> p.toAbsolutePath))
+      val filter = new Filter[Path]() {
+        override def accept(path: Path): Boolean = {
+          path.getFileName.toString.endsWith(".csv")
         }
-      )
-
-      val addresses = Flow[Iterable[(String, (String, Path))]].mapAsync(4) {
-        case cityDataSet ⇒
-          val map: Iterable[Future[Seq[Address]]] = cityDataSet.map {
-            case (state, (city, cityDataPath)) ⇒
-              val bytesToString = Flow[ByteString]
-                .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 256, allowTruncation = true))
-                .filter(_.nonEmpty)
-                .map(_.utf8String)
-
-              val toEntity = Flow.fromGraph(GraphDSL.create() { implicit b ⇒
-                val flow = b.add(
-                  Flow[String]
-                    .map(x ⇒ Try {
-                      val Array(lon, lat, number, street, unit, _, district, _, postcode, _, _) = x.split(",")
-                      Address(lon, lat, number, street, unit, city, district, state, postcode)
-                    })
-                    .filter(_.isSuccess)
-                    .map(_.get)
-                )
-                FlowShape(flow.in, flow.out)
-              })
-
-              FileIO
-                .fromPath(cityDataPath)
-                .via(bytesToString)
-                .drop(1)
-                .via(toEntity)
-                .runWith(Sink.seq)
-          }
-          Future.sequence(map).map(_.flatten)
       }
+      Files.newDirectoryStream(statePath, filter).map(p ⇒ country -> (state -> p.toAbsolutePath))
+    }
+  )
 
-      val countFuture = Source.single(Paths.get("/home/focusj/workspace/datasource/us"))
-        .via(citiesData)
-        .via(addresses)
-        .runFold(0)((r, e) ⇒ r + e.size)
+  val addresses = Flow[Iterable[(String, (String, Path))]].mapAsync(4) {
+    case cityDataSet ⇒
+      val map: Iterable[Future[Seq[Address]]] = cityDataSet.map {
+        case (state, (city, cityDataPath)) ⇒
+          val bytesToString = Flow[ByteString]
+            .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = 256, allowTruncation = true))
+            .filter(_.nonEmpty)
+            .map(_.utf8String)
 
-      countFuture.onComplete(x ⇒ println(s"total address: $x"))
+          val toEntity = Flow.fromGraph(GraphDSL.create() { implicit b ⇒
+            val flow = b.add(
+              Flow[String]
+                .map(x ⇒ Try {
+                  val Array(lon, lat, number, street, unit, _, district, _, postcode, _, _) = x.split(",")
+                  Address(lon, lat, number, street, unit, city, district, state, postcode)
+                })
+                .filter(_.isSuccess)
+                .map(_.get)
+            )
+            FlowShape(flow.in, flow.out)
+          })
 
-      ClosedShape
-    }).run()
+          FileIO
+            .fromPath(cityDataPath)
+            .via(bytesToString)
+            .drop(1)
+            .via(toEntity)
+            .runWith(Sink.seq)
+      }
+      Future.sequence(map).map(_.flatten)
+  }
+
+  val countFuture = Source.single(Paths.get("/home/focusj/workspace/datasource/us"))
+    .via(citiesData)
+    .via(addresses)
+    .runFold(0)((r, e) ⇒ r + e.size)
+
+  val start = System.currentTimeMillis()
+  countFuture.onComplete(x ⇒ {
+    println(s"there are address: $x address")
+    println(s"total cost is: ${System.currentTimeMillis() - start}")
+    system.terminate()
+  })
 
 }
